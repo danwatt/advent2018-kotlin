e57d514f5ca742a87e8f57a20c26af4c290a3b33
Dan Watt <danwatt@201online.com>
2018-12-05 15:59:05 -0600

    Add part 2 for day 1

--- src/main/kotlin/org/danwatt/Day1.kt
+++ src/main/kotlin/org/danwatt/Day1.kt
@@ -4,8 +4,25 @@ import java.io.File
 
 class Day1 : Day {
     override fun compute(): Int =
-        File(ClassLoader.getSystemResource("day1.txt").file)
-            .readLines()
-            .map { Integer.parseInt(it) }
-            .sum()
+        loadLines().sum()
+
+    private fun loadLines() = File(ClassLoader.getSystemResource("day1.txt").file)
+        .readLines().map { Integer.parseInt(it) }
+
+    fun computePartTwo(): Int = firstRepeat(loadLines())
+
+    fun firstRepeat(ints: List<Int>): Int {
+        val encountered = mutableSetOf<Int>()
+        var workingFrequency = 0;
+        while (true) {
+            ints.forEach {
+                encountered.add(workingFrequency)
+                workingFrequency += it
+                if (encountered.contains(workingFrequency)) {
+                    return workingFrequency
+                }
+            }
+        }
+        //Note this is unreachable
+    }
 }
